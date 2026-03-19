db3a85ebb759b5cc7c7493ef85d6a14afb89c0bd
Vitor Cesar <vitorcesarglima05@gmail.com>
2026-03-19 02:55:58 -0300

    feat: adicionei a pasta Data
    é a ponte de comunicação entre o código C# e o banco SQLite
    
    chore: adicionei minha estrategia de ter utilizado a pasta Data

---
+++ CatalogoProdutosAPI/Data/AppDataContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using CatalogoProdutosAPI.Models;
+
+namespace CatalogoProdutosAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Preco)
+                .HasPrecision(18, 2);
+        }
+    }
+}
